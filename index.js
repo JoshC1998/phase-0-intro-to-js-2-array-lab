aeeca7303fa37721cc9ab70fa93168a94e3c20c9
Joshua Cruz <jcruz1998@gmail.com>
2024-05-19 23:45:36 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    }
+    function destructivelyPrependCat(name) {
+        cats.unshift(name);
+        } 
+        function destructivelyRemoveLastCat(name) {
+            cats.pop(name);
+            } 
+            function destructivelyRemoveFirstCat(name) {
+                cats.shift(name);
+                }  
+                function appendCat(name) {
+                    // Declare an empty array
+                    const newCatsArray = [];
+                  
+                    // Use the spread operator to append the new cat
+                    newCatsArray.push(...cats, name);
+                  
+                    // Return the new array
+                    return newCatsArray;
+                  }
+                  function prependCat(name) {
+                    // Declare an empty array
+                    const newCatsArray = [];
+                  
+                    // Use the spread operator to append the new cat
+                    newCatsArray.push(name, ...cats);
+                  
+                    // Return the new array
+                    return newCatsArray;
+                  }
+                  function removeLastCat () {
+                  const newCatsArray = cats.slice(0, -1);
+                  return newCatsArray; 
+                }
+                function removeFirstCat () {
+                  const newCatsArray = cats.slice(1);
+                  return newCatsArray; 
+                }
+          
